207af9273e4f96e5e000b823dbcd19dacd5ee623
eliterunning54-hub <eliterunning54@gmail.com>
2026-02-11 19:21:39 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import WhatsAppButton from './components/WhatsAppButton';
+import Chatbot from './components/Chatbot';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Refit from './pages/Refit';
+import Rigging from './pages/Rigging';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+import FAQ from './pages/FAQ';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/servicos" element={<Services />} />
+            <Route path="/refit" element={<Refit />} />
+            <Route path="/rigging" element={<Rigging />} />
+            <Route path="/sobre" element={<About />} />
+            <Route path="/galeria" element={<Gallery />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/contato" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+        <WhatsAppButton />
+        <Chatbot />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
