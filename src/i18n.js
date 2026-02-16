c1c69572565396aa0873041323e99bdd51d19a59
eliterunning54-hub <eliterunning54@gmail.com>
2026-02-16 09:29:54 +0100

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,41 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      pt: {
+        translation: {
+          "nav.home": "Home",
+          "nav.services": "Serviços",
+          "nav.refit": "Refit & Reparos",
+          "nav.rigging": "Rigging",
+          "nav.about": "Sobre Nós",
+          "nav.gallery": "Galeria",
+          "nav.contact": "Contato",
+          "cta.quote": "Orçamento"
+        }
+      },
+      en: {
+        translation: {
+          "nav.home": "Home",
+          "nav.services": "Services",
+          "nav.refit": "Refit & Repairs",
+          "nav.rigging": "Rigging",
+          "nav.about": "About Us",
+          "nav.gallery": "Gallery",
+          "nav.contact": "Contact",
+          "cta.quote": "Get a Quote"
+        }
+      }
+    },
+    fallbackLng: 'pt',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
