c7ca46030f987b3e7efeb2885421591bf18e4dbc
eliterunning54-hub <eliterunning54@gmail.com>
2026-02-18 12:16:41 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -165,13 +165,12 @@ const Home = () => {
 
       {/* Banner igual ao Hero */}
       <section
-        className="relative w-full h-[500px] overflow-hidden"
+        className="relative w-full h-screen overflow-hidden"
         style={{
           backgroundImage:
             'url(https://hnaezacbzcpmyfoupdec.supabase.co/storage/v1/object/public/ANTARES%20ENERGIA/DM%20BANNER%20VELEIRO.webp)',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-          backgroundAttachment: 'fixed'
         }}
       >
         <div className="absolute inset-0 bg-navy-900/10"></div>
